efd1b8974fd9a0e1f889e5c7ec274ba6ba303e8f
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-07-05 12:09:08 -0300

    first commit

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-tooltip class="tooltip bg-grey-10 text-body1">
+    <slot />
+  </q-tooltip>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.tooltip {
+  box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.5);
+  border-radius: 50px;
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-icon
+      name="help"
+      size="xs"
+      class="cursor-pointer"
+    >
+      <tooltip
+        :model-value="true"
+      >
+      Teste teste
+      </tooltip>
+    </q-icon>
+  </q-page>
+</template>
+
+<script>
+import Tooltip from '../components/Tooltip.vue';
+
+export default {
+  name: 'PageIndex',
+  components: {
+    Tooltip,
+  },
+};
+</script>
