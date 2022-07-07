784d1c077bb36c129a8ab6ee5b72008ab00d0c94
Lucas Pina <ti.lucaspina@gmail.com>
2022-07-07 20:47:22 -0300

    ajustes

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,14 +1,16 @@
 <template>
   <q-tooltip
     content-class="tooltip"
-    max-width="400px"
     anchor="top middle"
     self="bottom middle"
-    offset="[10, 10]"
+    :offset="[15, 15]"
+    max-width="350px"
     transition-show="scale"
     transition-hide="scale"
   >
-    <div class="tooltip-text bg-grey-10 text-body2 text-justify no-scroll relative-position">
+    <div
+      class="tooltip-text bg-grey-10 text-caption text-justify relative-position"
+    >
       <slot />
     </div>
     <div class="tooltip-arrow bg-grey-10 absolute"></div>
@@ -16,13 +18,7 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      showing: true,
-    };
-  },
-};
+export default {};
 </script>
 
 <style lang="scss">
@@ -31,14 +27,14 @@ $box-shadow: 0px 0px 30px 0 rgba(0, 0, 0, 0.5);
 .tooltip {
   box-shadow: $box-shadow;
   background: transparent;
-  border-radius: 75px;
+  border-radius: 25%;
   overflow: visible;
   padding: 0;
 
   &-text {
     z-index: 10;
     box-shadow: $box-shadow;
-    border-radius: 75px;
+    border-radius: 25%;
     padding: 30px 30px 10px 30px;
   }
 
