1468f410461d12fdf6f47ef8daf5ee33a29f9433
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-07-07 12:09:45 -0300

    continuação e estudar alguns conceitos

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,16 +1,54 @@
 <template>
-  <q-tooltip class="tooltip bg-grey-10 text-body1">
-    <slot />
+  <q-tooltip
+    content-class="tooltip"
+    max-width="400px"
+    anchor="top middle"
+    self="bottom middle"
+    offset="[10, 10]"
+    transition-show="scale"
+    transition-hide="scale"
+  >
+    <div class="tooltip-text bg-grey-10 text-body2 text-justify no-scroll relative-position">
+      <slot />
+    </div>
+    <div class="tooltip-arrow bg-grey-10 absolute"></div>
   </q-tooltip>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      showing: true,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+$box-shadow: 0px 0px 30px 0 rgba(0, 0, 0, 0.5);
+
 .tooltip {
-  box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.5);
-  border-radius: 50px;
+  box-shadow: $box-shadow;
+  background: transparent;
+  border-radius: 75px;
+  overflow: visible;
+  padding: 0;
+
+  &-text {
+    z-index: 10;
+    box-shadow: $box-shadow;
+    border-radius: 75px;
+    padding: 30px 30px 10px 30px;
+  }
+
+  &-arrow {
+    z-index: 5;
+    width: 30px;
+    height: 30px;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%) rotate(-45deg) skew(-20deg, -20deg);
+  }
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,19 @@
 <template>
   <q-page class="flex flex-center">
-    <q-icon
-      name="help"
-      size="xs"
-      class="cursor-pointer"
-    >
-      <tooltip
-        :model-value="true"
-      >
-      Teste teste
+    <q-icon name="help" size="xs" class="cursor-pointer">
+      <tooltip>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+          inventore ducimus quod incidunt ratione tempore accusantium provident
+          dolor asperiores amet dolore repudiandae iste natus eveniet quae
+          perspiciatis cumque, odit dicta.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora quae
+          unde dolor? Magni, accusantium omnis? Recusandae blanditiis nisi nulla
+          delectus placeat perspiciatis nam, tenetur sunt totam animi dolores
+          non. Architecto?
+        </p>
       </tooltip>
     </q-icon>
   </q-page>
