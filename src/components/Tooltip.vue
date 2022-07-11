e164361559db1df1927d9df2b673eaf1f26bb638
Lucas <lucas.pina@prodepa.pa.gov.br>
2022-07-11 11:53:22 -0300

    arrows, top, left and right

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,10 +1,10 @@
 <template>
   <q-tooltip
-    content-class="tooltip"
+    content-class="tooltip tooltip-arrow-right-bottom"
     anchor="top middle"
     self="bottom middle"
     :offset="[15, 15]"
-    max-width="350px"
+    max-width="380px"
     transition-show="scale"
     transition-hide="scale"
   >
@@ -22,7 +22,7 @@ export default {};
 </script>
 
 <style lang="scss">
-$box-shadow: 0px 0px 30px 0 rgba(0, 0, 0, 0.5);
+$box-shadow: 0px 0px 30px 0 rgba(0, 0, 0, 0.2);
 
 .tooltip {
   box-shadow: $box-shadow;
@@ -42,9 +42,65 @@ $box-shadow: 0px 0px 30px 0 rgba(0, 0, 0, 0.5);
     z-index: 5;
     width: 30px;
     height: 30px;
-    bottom: 0;
-    left: 50%;
     transform: translateX(-50%) rotate(-45deg) skew(-20deg, -20deg);
+
+    // top arrows
+    [class*="tooltip-arrow-top"] & {
+      top: 0;
+    }
+    .tooltip-arrow-top & {
+      left: 50%;
+    }
+    .tooltip-arrow-top-left & {
+      left: 50px;
+    }
+    .tooltip-arrow-top-right & {
+      right: 23px;
+    }
+
+    // bottom arrows
+    [class*="tooltip-arrow-bottom"] & {
+      bottom: 0;
+    }
+    .tooltip-arrow-bottom & {
+      left: 50%;
+    }
+    .tooltip-arrow-bottom-left & {
+      left: 50px;
+    }
+    .tooltip-arrow-bottom-right & {
+      right: 40px;
+    }
+
+    // left arrows
+    [class*="tooltip-arrow-left"] & {
+      transform: translateY(-50%) rotate(45deg) skew(-20deg, -20deg);
+      left: 0;
+    }
+    .tooltip-arrow-left & {
+      top: 50%;
+    }
+    .tooltip-arrow-left-top & {
+      top: 50px;
+    }
+    .tooltip-arrow-left-bottom & {
+      bottom: 20px;
+    }
+
+    // right arrows
+    [class*="tooltip-arrow-right"] & {
+      transform: translateY(-50%) rotate(45deg) skew(-20deg, -20deg);
+      right: 0;
+    }
+    .tooltip-arrow-right & {
+      top: 50%;
+    }
+    .tooltip-arrow-right-top & {
+      top: 50px;
+    }
+    .tooltip-arrow-right-bottom & {
+      bottom: 20px;
+    }
   }
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <q-icon name="help" size="xs" class="cursor-pointer">
+    <q-icon name="help" size="xs" class="cursor-pointer" color="grey">
       <tooltip>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
           inventore ducimus quod incidunt ratione tempore accusantium provident
-          dolor asperiores amet dolore repudiandae iste natus eveniet quae
-          perspiciatis cumque, odit dicta.
         </p>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora quae
           unde dolor? Magni, accusantium omnis? Recusandae blanditiis nisi nulla
-          delectus placeat perspiciatis nam, tenetur sunt totam animi dolores
-          non. Architecto?
         </p>
       </tooltip>
     </q-icon>
